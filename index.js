c7dceb7973bea2a2a5381c6f188887833a2161e6
KAKADIYA VAIRAG <vairag.synfinity@gmail.com>
2025-07-28 18:31:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,9 @@ app.get('/', (req, res) => {
 });
 
 connectDB();
-socketHandlerChat(io);
-socketHandlerAudio(io);
+// socketHandlerChat(io);
+// socketHandlerAudio(io);
+socketHandlerMultiUserGroupVoiceCall(io)
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
